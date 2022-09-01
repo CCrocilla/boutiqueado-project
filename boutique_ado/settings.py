117f47c101190ff37387f613cfcd3b35ceefdac0
CCrocilla <cla.crocilla@gmail.com>
2022-09-01 00:44:56 +0000

    Create Base.html file and add block, add Allauth Files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -59,9 +59,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'django.contrib.sites',
-    
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
